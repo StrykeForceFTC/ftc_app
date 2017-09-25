fa2fd02f011aeb053b7897a6e562de20e70f66c2
elCapitan4 <sidnewtonian@gmail.com>
2017-09-25 19:21:15 -0400

    basic claw structure

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
@@ -3,6 +3,25 @@ package org.firstinspires.ftc.teamcode;
 /**
  * Created by gstaats on 21/09/17.
  */
+//Hello!
+import com.qualcomm.robotcore.hardware.Servo;
 
-public class Claw {
-}
+//Declaration of Servos
+public class claw {
+private Servo leftClawMotor;
+private Servo rightClawMotor;
+
+//
+//Claw inward motion
+public void claw_Inward(){
+        leftClawMotor.setPower(1);
+        rightClawMotor.setPower(1);
+        }
+//Claw outward motion
+public void claw_Outward(){
+        leftClawMotor.setPower(-1);
+        rightClawMotor.setPower(-1);
+
+        }
+
+        }
