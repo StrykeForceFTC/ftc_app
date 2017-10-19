da718730b89b26360ab3c575295886a0c484402d
elCapitan4 <sidnewtonian@gmail.com>
2017-10-19 18:27:41 -0400

    Hardware Map added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
@@ -4,16 +4,15 @@ package org.firstinspires.ftc.teamcode;
  * Created by gstaats on 21/09/17.
  */
 //Hello!
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.HardwareMap
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 //Declaration of Servos
-public class claw {
-private Servo leftClawMotor = null;
-private Servo rightClawMotor = null;
+public class Claw {
+        private Servo leftClawMotor = null;
+        private Servo rightClawMotor = null;
 
-        public claw( HardwareMap ahwMap )
+        public Claw( HardwareMap ahwMap )
         {
                 //Hardware Map
                 rightClawMotor = ahwMap.servo.get( "rightClawMotor" );
@@ -28,17 +27,17 @@ private Servo rightClawMotor = null;
                 leftClawMotor.setPower( 0 );
         }
 
-//
+        //
 //Claw inward motion
-public void claw_Inward(){
-        leftClawMotor.setPower(1);
-        rightClawMotor.setPower(-1);
+        public void claw_Inward(){
+                leftClawMotor.setPower(0.1);
+                rightClawMotor.setPower(-0.1);
         }
-//Claw outward motion
-public void claw_Outward(){
-        leftClawMotor.setPower(-1);
-        rightClawMotor.setPower(1);
+        //Claw outward motion
+        public void claw_Outward(){
+                leftClawMotor.setPower(-0.1);
+                rightClawMotor.setPower(0.1);
 
         }
 
-        }
+}
