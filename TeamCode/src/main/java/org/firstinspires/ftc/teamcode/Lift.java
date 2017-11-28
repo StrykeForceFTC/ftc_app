ab25cd922dc489b3779c72205f56eb13f7834a00
jefflscott <jeff_l_scott@yahoo.com>
2017-11-28 16:14:22 -0500

    Force lift power to 0 when it gets near zero and that is what the user is requesting, and reduce lift power during auton for the last 5 loops

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lift.java
@@ -28,6 +28,10 @@ public class Lift {
     final private static double AUTON_TIME_PER_PASS_MS = 20.0;
     final private static int AUTON_LOOPS_4_RAISE = (int) ( AUTON_RAISE_TIME_MS / AUTON_TIME_PER_PASS_MS );
     final private static int AUTON_LOOPS_4_LOWER = (int) ( AUTON_LOWER_TIME_MS / AUTON_TIME_PER_PASS_MS );
+    final private static double AUTON_RAMP_FACTOR = 0.2;
+
+    // Lower limit for lift power
+    final private static double LIFT_POWER_LOWER_LIMIT = 0.02;
 
     // Lift motor power - must be a class member to allow filtering
     private double liftMotorPower = 0.0;
@@ -49,6 +53,14 @@ public class Lift {
     public void Raise( double raiseLower )
     {
         liftMotorPower = JoystickUtilities.LowPassFilter( liftMotorPower, raiseLower, INCREASING_FILTER_K, DECREASING_FILTER_K );
+
+        // If input near 0, then force value to 0 if mag < LIFT_POWER_LOWER_LIMIT
+        double liftMagnitude = Math.abs( liftMotorPower );
+        if ( ( liftMagnitude <= LIFT_POWER_LOWER_LIMIT ) && ( Math.abs( raiseLower ) <= 0.01 ) )
+        {
+            liftMotorPower = 0.0;
+        }
+
         RaiseUnfiltered( liftMotorPower );
     }
 
@@ -63,8 +75,16 @@ public class Lift {
         double liftPower = 0.0;
         for (int loop = 0; loop < AUTON_LOOPS_4_RAISE; loop++)
         {
-            // Ramp power up
-            liftPower = RampPower( AUTON_LIFT_POWER, liftPower );
+            // Ramp power up to max and hold until last few loops
+            if ( loop < ( AUTON_LOOPS_4_RAISE - 5 ) )
+            {
+                liftPower = RampPower(AUTON_LIFT_POWER, liftPower);
+            }
+            else
+            {
+                // On last 5 loops, so lower power to half
+                liftPower = AUTON_LIFT_POWER / 2.0;
+            }
 
             // To raise, power is negative
             RaiseUnfiltered( -liftPower );
@@ -83,8 +103,16 @@ public class Lift {
         double liftPower = 0.0;
         for (int loop = 0; loop < AUTON_LOOPS_4_LOWER; loop++)
         {
-            // Ramp power up
-            liftPower = RampPower( AUTON_LIFT_POWER, liftPower );
+            // Ramp power up and then hold until last 5 loops
+            if ( loop < ( AUTON_LOOPS_4_LOWER - 5 ) )
+            {
+                liftPower = RampPower(AUTON_LIFT_POWER, liftPower);
+            }
+            else
+            {
+                // Just set power to half for last 5 loops
+                liftPower = AUTON_LIFT_POWER / 2.0;
+            }
 
             // To lower, power is positive
             RaiseUnfiltered( liftPower );
@@ -97,10 +125,10 @@ public class Lift {
         RaiseUnfiltered( 0.0 );
     }
 
-    // Method to ramp power. Adds in 20% each pass
+    // Method to ramp power. Adds in AUTON_RAMP_FACTOR * target each pass
     private double RampPower( double target, double now )
     {
-        double updated = now + 0.2 * target;
+        double updated = now + ( AUTON_RAMP_FACTOR * target );
         if ( updated > target )
         {
             updated = target;
