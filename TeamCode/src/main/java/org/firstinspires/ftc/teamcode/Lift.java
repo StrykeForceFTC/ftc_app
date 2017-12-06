bf7938841018c7e04a960a4dbab1b24e2567d00f
jefflscott <jeff_l_scott@yahoo.com>
2017-12-06 08:00:54 -0500

    Simplify auton raise / lower

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lift.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -23,12 +24,8 @@ public class Lift {
     // we raise to avoid bottoming out the motor. Delay is actually done in
     // shorter segments to allow ramping of motor power.
     final private static double AUTON_LIFT_POWER = 0.5;
-    final private static double AUTON_RAISE_TIME_MS = 400.0;
-    final private static double AUTON_LOWER_TIME_MS = AUTON_RAISE_TIME_MS - 80.0;
-    final private static double AUTON_TIME_PER_PASS_MS = 20.0;
-    final private static int AUTON_LOOPS_4_RAISE = (int) ( AUTON_RAISE_TIME_MS / AUTON_TIME_PER_PASS_MS );
-    final private static int AUTON_LOOPS_4_LOWER = (int) ( AUTON_LOWER_TIME_MS / AUTON_TIME_PER_PASS_MS );
-    final private static double AUTON_RAMP_FACTOR = 0.2;
+    final private static double AUTON_RAISE_TIME_MS = 250.0;
+    final private static double AUTON_LOWER_TIME_MS = AUTON_RAISE_TIME_MS - 50.0;
 
     // Lower limit for lift power
     final private static double LIFT_POWER_LOWER_LIMIT = 0.02;
@@ -72,27 +69,11 @@ public class Lift {
     // Method to raise the lift a set amount in auton mode
     public void AutonRaise( )
     {
-        double liftPower = 0.0;
-        for (int loop = 0; loop < AUTON_LOOPS_4_RAISE; loop++)
-        {
-            // Ramp power up to max and hold until last few loops
-            if ( loop < ( AUTON_LOOPS_4_RAISE - 5 ) )
-            {
-                liftPower = RampPower(AUTON_LIFT_POWER, liftPower);
-            }
-            else
-            {
-                // On last 5 loops, so lower power to half
-                liftPower = AUTON_LIFT_POWER / 2.0;
-            }
+        // To raise, power is negative
+        RaiseUnfiltered( -AUTON_LIFT_POWER );
 
-            // To raise, power is negative
-            RaiseUnfiltered( -liftPower );
-
-            // Delay is used to give time for motor to go
-            // to new power and to control over all raise time
-            JoystickUtilities.Delay_ms( AUTON_TIME_PER_PASS_MS );
-        }
+        // Give time to raise
+        JoystickUtilities.Delay_ms( AUTON_RAISE_TIME_MS );
 
         RaiseUnfiltered( 0.0 );
     }
@@ -100,41 +81,13 @@ public class Lift {
     // Method to lower the lift a set amount in auton mode
     public void AutonLower( )
     {
-        double liftPower = 0.0;
-        for (int loop = 0; loop < AUTON_LOOPS_4_LOWER; loop++)
-        {
-            // Ramp power up and then hold until last 5 loops
-            if ( loop < ( AUTON_LOOPS_4_LOWER - 5 ) )
-            {
-                liftPower = RampPower(AUTON_LIFT_POWER, liftPower);
-            }
-            else
-            {
-                // Just set power to half for last 5 loops
-                liftPower = AUTON_LIFT_POWER / 2.0;
-            }
+        // To lower, power is positive
+        RaiseUnfiltered( AUTON_LIFT_POWER );
 
-            // To lower, power is positive
-            RaiseUnfiltered( liftPower );
-
-            // Delay is used to give time for motor to go
-            // to new power and to control over all raise time
-            JoystickUtilities.Delay_ms( AUTON_TIME_PER_PASS_MS );
-        }
+        // Give time to raise
+        JoystickUtilities.Delay_ms( AUTON_LOWER_TIME_MS );
 
         RaiseUnfiltered( 0.0 );
-    }
-
-    // Method to ramp power. Adds in AUTON_RAMP_FACTOR * target each pass
-    private double RampPower( double target, double now )
-    {
-        double updated = now + ( AUTON_RAMP_FACTOR * target );
-        if ( updated > target )
-        {
-            updated = target;
-        }
-
-        return updated;
     }
 
     public int GetPosition( )
