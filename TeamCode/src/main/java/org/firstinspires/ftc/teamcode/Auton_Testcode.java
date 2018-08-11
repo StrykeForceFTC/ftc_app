62f380c711f41149c4fd404593de9e32679ddb32
jefflscott <jeff_l_scott@yahoo.com>
2018-08-11 09:36:59 -0400

    Fixed joystick Y processing in Auton_Testcode and corrected Drive to match. Also added auton methods to move robot using encoders for FWD, REV, RIGHT and LEFT

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton_Testcode.java
@@ -0,0 +1,111 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+/**
+ * Created by jscott on 11/10/17.
+ *
+ * Auton Program mainly here to test Auton Drive and other Functions in
+ *
+ */
+@Autonomous(name = "Auton_Testcode", group = "Linear Opmode")
+//@Disabled
+public class Auton_Testcode extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private DcMotor frontLeft = null;
+    private DcMotor frontRight = null;
+    private DcMotor rearLeft = null;
+    private DcMotor rearRight = null;
+
+    HardwareMap robotMap = hardwareMap;
+    private Drive go = null;
+
+    // Enumeration for auton steps
+    private enum AUTON_STEPS { START, PICK_UP_GLYPH, KNOCK_OFF_JEWEL,
+        MOVE_IN_FRONT_OF_BOX, ROTATE_TO_FACE_BOX, MOVE_FORWARD_TO_BOX,
+        DROP_GYLPH, BACK_UP, STOP }
+
+    private AUTON_STEPS step = AUTON_STEPS.START;
+
+    // Constants for controlling / tuning auton, long distances reduced by 1"
+    // to make up for error we have seen. All values in cm or degrees
+    private static final double DISTANCE_FOR_LEFT_COLUMN = ( 72.07 - 2.54 );     //
+    private static final double DISTANCE_FOR_CENTER_COLUMN = ( 91.44 - 2.54 );   //
+    private static final double DISTANCE_FOR_RIGHT_COLUMN = ( 110.81 - 2.54 );   //
+    private static final double DEGREES_2_ROTATE = 90.0;              // Must rotate CCW
+    private static final double DISTANCE_FORWARD_2_DROP = 20.32;      // Leave long, will just run into wall
+    private static final double DISTANCE_FOR_JEWEL = 9.21;            // Distance to move to knock off a jewel
+
+    // Used to compensate for movement to knock off jewel
+    private double yDistanceFromStart = 0.0;
+
+    /*
+     * There is only runOpMode for linear op modes
+     */
+    @Override
+    public void runOpMode()
+    {
+        /*
+        **  Initialization code
+         */
+        telemetry.addData("Status", "Initializing");
+
+        /* eg: Initialize the hardware variables. Note that the strings used here as parameters
+         * to 'get' must correspond to the names assigned during the robot configuration
+         * step (using the FTC Robot Controller app on the phone).
+         */
+        frontLeft = hardwareMap.dcMotor.get("front_left");
+        frontRight = hardwareMap.dcMotor.get("front_right");
+        rearLeft = hardwareMap.dcMotor.get("rear_left");
+        rearRight = hardwareMap.dcMotor.get("rear_right");
+
+        go = new Drive(frontLeft, frontRight, rearLeft, rearRight);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Wait hit till start button pressed
+        waitForStart();
+        runtime.reset();
+
+        // Create rotate adjustment outside of while loop so it doesn't get
+        // reset each pass of the loop
+        double rotateAdjustment = 0.0;
+
+        // Loop until stop or forced to end
+        while ( opModeIsActive() )
+        {
+            int errorTicks = 0;
+         go.AutonMove(Drive.DIRECTION.FORWARD, 48);
+
+
+            telemetry.addData( "Movement Error ", errorTicks );
+
+            telemetry.addLine("Encoders ")
+                    .addData("FL ", frontLeft.getCurrentPosition() )
+                    .addData("FR ", frontRight.getCurrentPosition() );
+
+            telemetry.update();
+
+            idle();
+        }
+    }
+
+}
+
+
