0b81358a7698b810cebf4e8f7b424d14ced35805
J Scott <jeff_l_scott@yahoo.com>
2018-11-09 20:24:06 -0500

    Refactor initial auton work on the facing depot side from auton test code to Auton_Facing_Crater and put common code into AutonLinearBase. Created Auton_Facing_Depot from Auton_Facing_Crater and modify move to depot method for different position.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonLinearBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonLinearBase.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode;
 import com.disnodeteam.dogecv.CameraViewDisplay;
 import com.disnodeteam.dogecv.DogeCV;
 import com.disnodeteam.dogecv.detectors.roverrukus.GoldAlignDetector;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -16,7 +15,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  */
 public abstract class AutonLinearBase extends LinearOpMode
 {
-
+    // Run time tracker
     public ElapsedTime runtime = new ElapsedTime();
 
     // Declare HW objects
@@ -28,7 +27,56 @@ public abstract class AutonLinearBase extends LinearOpMode
     // Detectors
     private GoldAlignDetector detector;
 
+    // Team ID
     public Auto_Robot_Detect.teamId TeamId = Auto_Robot_Detect.teamId.teamUnknown;
+
+    // Auton steps
+    // Enumeration for auton steps allowing move to next step
+    // using Next
+    protected enum AUTON_STEPS
+    {
+        RELEASE_LANDER,
+        FIND_GOLD,
+        MOVE_TO_MINERAL,
+        LOAD_GOLD,
+        DRIVE_DEPOT,
+        UNLOAD,
+        PARK,
+        STOP;
+
+        private static AUTON_STEPS[] vals = values();
+        public AUTON_STEPS Next( )
+        {
+            if ( this != STOP )
+            {
+                return vals[ ( this.ordinal() + 1 ) ];
+            }
+
+            return STOP;
+        }
+
+    }
+
+    // Enumeration for possible positions of the gold mineral
+    protected enum GOLD_POSITIONS {
+        LEFT_POS,
+        MID_POS,
+        RIGHT_POS,
+        UNKNOWN_POS
+    }
+
+    // Variables for step we are on and position of gold mineral
+    protected AUTON_STEPS step = AUTON_STEPS.RELEASE_LANDER;
+    protected GOLD_POSITIONS gold = GOLD_POSITIONS.UNKNOWN_POS;
+
+    // Values for distances to move, degrees to rotate, etc.
+    protected double FIND_GOLD_INITIAL_CW_ROT_DEG = 135.0;
+    protected double FIND_GOLD_ROTATE_4_SAMPLE_IN = 45.0;
+    protected double GO_TO_GOLD_FWD_IN = 8.0;
+    protected double GO_TO_GOLD_SIDEWAYS_IN = 8.0;
+    protected double LOAD_GOLD_FWD_IN = 4.0;
+    protected double PARK_DISTANCE_IN = 54.0;
+
 
     // Method to initialize any connected hardware
     public void InitHardware( )
@@ -95,6 +143,126 @@ public abstract class AutonLinearBase extends LinearOpMode
         // Next line indicates to robot core that we are requesting
         // to stop the op mode (like hitting stop button on driver station)
         requestOpModeStop( );
+    }
+
+    /*
+    ** Methods common to all auton routines.
+    */
+    protected void ReleaseLander( )
+    {
+        //! @todo Need to implement releasing from lander
+    }
+
+    // Method to find position of gold mineral.
+    protected void FindGold( )
+    {
+        // Initial rotation to align with sample on left when facing crater or depot
+        go.AutonMoveRotate( Drive.ROTATION.CLOCKWISE, FIND_GOLD_INITIAL_CW_ROT_DEG );
+
+        if ( GoldAligned() )
+        {
+            gold = GOLD_POSITIONS.LEFT_POS;
+            go.AutonMoveRotate(Drive.ROTATION.CLOCKWISE, FIND_GOLD_ROTATE_4_SAMPLE_IN );
+
+        }
+        else
+        {
+            go.AutonMoveRotate( Drive.ROTATION.CLOCKWISE, FIND_GOLD_ROTATE_4_SAMPLE_IN );
+            if ( GoldAligned() )
+            {
+                gold = GOLD_POSITIONS.MID_POS;
+
+            }
+            else
+            {
+                go.AutonMoveRotate(Drive.ROTATION.CLOCKWISE, FIND_GOLD_ROTATE_4_SAMPLE_IN );
+                if ( GoldAligned() )
+                {
+                    gold = GOLD_POSITIONS.RIGHT_POS;
+                    go.AutonMoveRotate(Drive.ROTATION.COUNTERCLOCKWISE, FIND_GOLD_ROTATE_4_SAMPLE_IN );
+                }
+                else
+                {
+                    gold = GOLD_POSITIONS.UNKNOWN_POS;
+                    go.AutonMoveRotate(Drive.ROTATION.COUNTERCLOCKWISE, FIND_GOLD_ROTATE_4_SAMPLE_IN );
+
+                }
+            }
+
+        }
+    }
+
+    // Method to move to gold mineral position
+    protected void GoToGold( )
+    {
+        go.AutonMove( Drive.DIRECTION.FORWARD, GO_TO_GOLD_FWD_IN );
+
+        switch ( gold )
+        {
+            case LEFT_POS:
+                go.AutonMove( Drive.DIRECTION.LEFT, GO_TO_GOLD_SIDEWAYS_IN );
+                break;
+
+            case MID_POS:
+                break;
+
+            case RIGHT_POS:
+                go.AutonMove( Drive.DIRECTION.RIGHT, GO_TO_GOLD_SIDEWAYS_IN );
+                break;
+
+            case UNKNOWN_POS:
+                break;
+        }
+    }
+
+
+    // Method to load gold mineral
+    protected void LoadGold( )
+    {
+        // Only load if we found the gold mineral
+        if ( gold != GOLD_POSITIONS.UNKNOWN_POS )
+        {
+            go.AutonMove(Drive.DIRECTION.FORWARD, LOAD_GOLD_FWD_IN);
+
+            //! @todo Need arm method to put to load position
+            // arm.LoadSamplesPositionSet();
+            loader.AutonLoad();
+
+            //put down arm and turn on loader
+            go.AutonMove( Drive.DIRECTION.REVERSE, LOAD_GOLD_FWD_IN );
+
+            switch ( gold )
+            {
+                case LEFT_POS:
+                    go.AutonMove( Drive.DIRECTION.RIGHT, GO_TO_GOLD_SIDEWAYS_IN );
+                    break;
+
+                case MID_POS:
+                    break;
+
+                case RIGHT_POS:
+                    go.AutonMove( Drive.DIRECTION.LEFT, GO_TO_GOLD_SIDEWAYS_IN );
+                    break;
+            }
+        }
+    }
+
+    // The drive to depot method is expected to be different for each
+    // position, so each auton routine must override it.
+    abstract protected void DriveToDepot( );
+
+    // Common method to unload marker and gold sample
+    protected void UnloadGoldAndMarker( )
+    {
+        //! @todo Need method / object to drop marker
+        loader.AutonUnload();
+    }
+
+    // Method to get to park position
+    protected void ParkTheRobot( )
+    {
+        // Raise arm to move position?
+        go.AutonMove( Drive.DIRECTION.REVERSE, PARK_DISTANCE_IN );
     }
 
 }
