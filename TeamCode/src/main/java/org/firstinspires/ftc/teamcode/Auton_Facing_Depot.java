8a839a2c89ab4221dd1da02b7267642cf40ae04c
Jeff Scott <jeff_l_scott@yahoo.com>
2018-11-29 11:32:09 -0500

    Remove commented out line of code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton_Facing_Depot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton_Facing_Depot.java
@@ -150,7 +150,6 @@ public class Auton_Facing_Depot extends AutonLinearBase
                 {
                     // Sample gold mineral
                     GoToGold();
-//                    step = step.Next();
                     step = step.Next();
                     break;
                 }
