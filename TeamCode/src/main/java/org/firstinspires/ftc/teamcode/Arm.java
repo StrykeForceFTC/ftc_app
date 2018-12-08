106a7d486eb5c22405dc151a2ed3c79c4a9712ee
jefflscott <jeff_l_scott@yahoo.com>
2018-12-08 13:50:29 -0500

    tuning 7228 auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
@@ -166,7 +166,7 @@ public class Arm {
                 switch (TeamId)
                 {
                     case team7228:
-                        return 1300;
+                        return 1250;
                     case team8553:
                         return 600;
                     case team15106:
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton_Facing_Depot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton_Facing_Depot.java
@@ -87,8 +87,8 @@ public class Auton_Facing_Depot extends AutonLinearBase
                 DRIVE_DEPOT_MOVE_2_WALL_RIGHT_IN = 38;
 
                 DRIVE_DEPOT_ROT_PARALLEL_2_WALL_LEFT_DEG = 62;
-                DRIVE_DEPOT_ROT_PARALLEL_2_WALL_MID_DEG = 62;
-                DRIVE_DEPOT_ROT_PARALLEL_2_WALL_RIGHT_DEG = 55;
+                DRIVE_DEPOT_ROT_PARALLEL_2_WALL_MID_DEG = 65;
+                DRIVE_DEPOT_ROT_PARALLEL_2_WALL_RIGHT_DEG = 58;
 
                 DRIVE_DEPOT_FWD_2_DEPOT_LEFT_IN = 32;
                 DRIVE_DEPOT_FWD_2_DEPOT_MID_IN = 29;
